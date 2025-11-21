be632b754cb5fd71085fbe833cc1ad155fc17d96
Sajith Minrutha <minruthasajith@gmail.com>
2025-11-21 20:30:11 +0530

    bug fix

--- src/Validation.cpp
+++ src/Validation.cpp
@@ -8,17 +8,17 @@ int Validation::invaidInputCheck(string &t, string &a, string &I, int &q, double
 {
     if (t == "" || a == "" || I == "" || g == "" || pub == "")
     {
-        cout << "error";
+
         return 1;
     }
     else if (q < 0 || p < 0.0 || y < 0)
     {
-        cout << "error";
+
         return 1;
     }
     else
     {
-        cout << "Valid";
+
         return 0;
     }
 }
